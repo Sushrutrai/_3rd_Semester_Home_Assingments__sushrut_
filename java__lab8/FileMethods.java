9569cc606e49bf18cf476d16fbd2bf50d0d9b529
Sushrutrai <sushrutrai99@gmail.com>
2025-08-02 12:25:39 +0545

    oka

--- java__lab8/FileMethods.java
+++
@@ -1,8 +0,0 @@
-package java__lab8;
-import java.io.*;
-//  1.	Write a program to demonstrate various methods of File class.
-public class FileMethods{
-    public static void main(String[] args){
-        
-    }
-}
